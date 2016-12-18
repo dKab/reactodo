35aa2785f8d22399fd49aa4f0cb26f9ecf3dffb6
dmitriy <dkabardinov@gmail.com>
2016-12-19 00:42:57 +0300

    redux work in progress

--- src/components/search/search.component.js
+++ src/components/search/search.component.js
@@ -3,7 +3,7 @@ import './search.css';
 
 export default function Search(props) {
     return (<div className="search">
-        <label><input checked={props.filterActive} type="checkbox"/> Show active</label>
+        <label><input checked={props.filterActive} type="checkbox"/> Show done</label>
         <input placeholder="Search" type="text" className="search__input" /><span className="search__clear">&times;</span>
     </div>);
 }
